a878ffb8a28212d1f4b213bc6f00a758adce1565
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-03-03 11:26:45 +0100

    Add config file

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
 # Created by Kelvin_Clark on 3/3/2022, 11:06 AM
+from dotenv import load_dotenv
+
+load_dotenv()
--- src/config.py
+++ src/config.py
@@ -1,1 +1,34 @@
 # Created by Kelvin_Clark on 3/3/2022, 11:06 AM
+import os
+from flask import Flask
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    @staticmethod
+    def init_app(app: Flask):
+        pass
+
+
+class Development(Config):
+    DEBUG = True
+
+
+class Production(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+    SECRET_KEY = os.environ["SECRET_KEY"]
+
+
+class Test(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///./test.sqlite3"
+
+
+config = {
+    "production": Production,
+    "development": Development,
+    "testing": Test,
+    "default": Development
+}
