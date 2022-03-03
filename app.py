9530ebeb6f1552844ab6b899297c040a09ae271e
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-03-03 11:33:49 +0100

    Add app init function

--- app.py
+++ app.py
@@ -1,4 +1,1 @@
 # Created by Kelvin_Clark on 3/3/2022, 11:06 AM
-from dotenv import load_dotenv
-
-load_dotenv()
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,21 @@
 # Created by Kelvin_Clark on 3/3/2022, 11:06 AM
+from typing import Optional
+
+from dotenv import load_dotenv
+from flask import Flask
+
+from src.config import config
+
+load_dotenv()
+
+
+def create_app(config_name: Optional[str] = "default") -> Flask:
+    app = Flask(__name__)
+    try:
+        app.config.from_object(config[config_name])
+        config[config_name].init_app(app)
+    except KeyError:
+        raise ValueError(
+            f"The config name can be either default, development, production or testing not ${config_name}")
+
+    return app
