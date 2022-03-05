42905ab84f0f20186d45f9729fa0335fa1433343
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-03-05 18:41:12 +0100

    Create user dao
    Currently, it contains a function for getting the user by email

---
+++ src/dao/user.py
@@ -0,0 +1,11 @@
+# Created by Kelvin_Clark on 3/5/2022, 6:37 PM
+from typing import Optional
+
+from src.models.entities.user import User
+
+
+class UserDao:
+
+    @staticmethod
+    def get_user_by_email(email: str) -> Optional[User]:
+        return User.query.filter_by(User.email == email).first()
