a9845e862fc886b94e7883668f53148f2eabf2e5
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2022-03-06 19:19:28 +0100

    Create sign up form class

---
+++ src/models/forms/sign_up.py
@@ -0,0 +1,11 @@
+# Created by Kelvin_Clark on 3/6/2022, 7:08 PM
+from wtforms import PasswordField, StringField, validators
+from .sign_in import SignInForm
+
+
+class SignUpForm(SignInForm):
+    confirm_password = PasswordField("Confirm password", validators=[validators.input_required(),
+                                                                     validators.EqualTo(fieldname="password",
+                                                                                        message="Passwords don't match")
+                                                                     ])
+    username = StringField(label="Username", validators=[validators.input_required()])
