e46fe49270105a20776e79d78d4e54a0990a25df
lokaimoma <kelvinclark2k77@gmail.com>
2022-03-10 17:22:28 +0100

    Add test user fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,3 +15,16 @@ def app():
 @fixture()
 def test_client(app):
     return app.test_client()
+
+
+@fixture()
+def test_user(app):
+    with app.app_context():
+        from src import database
+        from src.models.entities.user import User
+        from src.utils.security.password_util import hash_password
+        user = User(username="tester1", email="test@qrweb.com", password=hash_password(password="password"))
+        database.session.add(user)
+        database.session.commit()
+        database.session.refresh(user)
+    return user
