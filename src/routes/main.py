629c21c27e36b6e9e86a6ef6d7fb0cc6935e366a
lokaimoma <kelvinclark2k77@gmail.com>
2022-03-10 13:57:42 +0100

    Create main app router

--- src/routes/main.py
+++ src/routes/main.py
@@ -1,9 +1,11 @@
 # Created by Kelvin_Clark on 3/10/22, 1:10 PM
-from flask import Blueprint, session, redirect
-
+from flask import Blueprint, session, redirect, render_template
+from .dependencies.session import login_required
 
 main_router = Blueprint("main", import_name=__name__)
 
-@main_router.route("/")
-def main_index():
 
+@main_router.route("/")
+@login_required
+def main_index():
+    return render_template("main/index.html")
