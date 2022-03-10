321bb56cf419e7710ed8939b1d07547f222f1603
lokaimoma <kelvinclark2k77@gmail.com>
2022-03-10 15:16:58 +0100

    Test login required

--- tests/test_main_router.py
+++ tests/test_main_router.py
@@ -1,9 +1,11 @@
 # Created by Kelvin_Clark on 3/10/22, 2:45 PM
 from flask import url_for
+import pytest
 
 
-def test_no_user(test_client):
+@pytest.mark.parametrize(argnames='path', argvalues=["/app/"])
+def test_login_required(test_client, path):
     with test_client:
-        response = test_client.get("/app/")
+        response = test_client.get(path)
         assert response.status_code == 302
         assert url_for("auth.login_route") in response.headers.get("Location")
