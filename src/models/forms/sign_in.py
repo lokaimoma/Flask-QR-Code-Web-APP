08f99aa4883f0b23cfa2c15b8a49af49ac5c4ba4
lokaimoma <kelvinclark2k77@gmail.com>
2022-03-13 17:38:29 +0100

    Add form placeholders

--- src/models/forms/sign_in.py
+++ src/models/forms/sign_in.py
@@ -3,5 +3,7 @@ from wtforms import Form, PasswordField, EmailField, validators
 
 
 class SignInForm(Form):
-    email = EmailField("Email", validators=[validators.input_required(), validators.email()])
-    password = PasswordField("Password", validators=[validators.input_required()])
+    email = EmailField("Email", validators=[validators.input_required(), validators.email()],
+                       render_kw={"placeholder": "Email"})
+    password = PasswordField("Password", validators=[validators.input_required()],
+                             render_kw={"placeholder": "Password"})
--- src/models/forms/sign_up.py
+++ src/models/forms/sign_up.py
@@ -4,8 +4,11 @@ from .sign_in import SignInForm
 
 
 class SignUpForm(SignInForm):
-    confirm_password = PasswordField("Confirm password", validators=[validators.input_required(),
-                                                                     validators.EqualTo(fieldname="password",
-                                                                                        message="Passwords don't match")
-                                                                     ])
-    username = StringField(label="Username", validators=[validators.input_required()])
+    confirm_password = PasswordField("Confirm password",
+                                     validators=[validators.input_required(),
+                                                 validators.EqualTo(fieldname="password",
+                                                                    message="Passwords don't match")
+                                                 ],
+                                     render_kw={"placeholder": "Confirm password"})
+    username = StringField(label="Username", validators=[validators.input_required()],
+                           render_kw={"placeholder": "Username"})
